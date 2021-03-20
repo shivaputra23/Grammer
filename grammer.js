3c325e18e527bb415af8f3c51e4fb8b5466a5c27
shivaputra23 <78872822+shivaputra23@users.noreply.github.com>
2021-03-20 17:41:15 +0530

    Update grammer.js

--- grammer.js
+++ grammer.js
@@ -1,11 +1,18 @@
-function loadFileAsText(){
-    var fileToLoad = document.getElementById("fileToLoad").files[0];
-
-    var fileReader = new FileReader();
-    fileReader.onload = function(fileLoadedEvent){
-        var textFromFileLoaded = fileLoadedEvent.target.result;
-        document.getElementById("inputTextToSave").value = textFromFileLoaded;
-    };
-
-    fileReader.readAsText(fileToLoad, "UTF-8");
-  }
+const input = document.querySelector('input')  
+const textarea = document.querySelector('textarea') 
+input.addEventListener('change', function () { 
+    let files = input.files; 
+    if (files.length == 0) return; 
+    let file = files[0]; 
+    var reader = new FileReader(); 
+    reader.onload = function (e) { 
+        const file = e.target.result; 
+        const lines = file.split(/\r\n|\n/); 
+        textarea.value = lines.join('\n'); 
+        fetch('https://api.textgears.com/spelling?key=1gVny1rfj02gy7kY&text='+ textarea.value)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }; 
+    reader.onerror = function(e) {alert(e.target.error.name)}; 
+    reader.readAsText(file);
+}); 
